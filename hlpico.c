08a87273abb59975784be4a837b21c353049645d
nevergarden <nevergarden.fa@gmail.com>
2023-07-31 17:02:44 +0330

    add: support for headers

--- hlpico.c
+++ hlpico.c
@@ -11,67 +11,53 @@
 
 typedef struct {
 	hl_type * t;
-	vbyte * name;
-	vbyte * value;
-} hlphr_header;
-
-typedef struct {
-	hl_type * t;
-	hlphr_header * headers;
-	int header_count;
-} hlphr_headers;
-
-typedef struct {
-	hl_type * t;
 	vbyte * method;
 	vbyte * path;
 	int request_size;
 	int minor_version;
-	hlphr_headers *headers;
+	int header_count;
+	varray *headers;
 } hlphr_data;
 
 int pret, minor_version;
 const char * method, *path;
-size_t method_len = 0, path_len = 0, num_headers = 0;
+size_t method_len = 0, path_len = 0, num_headers;
 
 HL_PRIM hlphr_data* HL_NAME(parse_request)(const vbyte * buf, int max_header) {
 	struct phr_header headers[max_header];
 	const char* cb = (char*) buf;
+	num_headers = sizeof(headers) / sizeof(headers[0]);
 	pret = phr_parse_request(cb, strlen(cb), &method, &method_len, &path, &path_len, &minor_version, headers, &num_headers, 0);
 
 	hlphr_data * ret = malloc(sizeof(hlphr_data));
-	char * method_s = (char*) hl_alloc_bytes(method_len);
+	char * method_s = (char*) hl_alloc_bytes(method_len+1);
 	strncpy(method_s, method, method_len);
-	char * path_s = (char*) hl_alloc_bytes(path_len);
+	char * path_s = (char*) hl_alloc_bytes(path_len+1);
 	strncpy(path_s, path, path_len);
-
+	
 	ret->method = (vbyte*) method_s;
 	ret->path = (vbyte*) path_s;
 	ret->minor_version = minor_version;
 	ret->request_size = pret;
+	ret->header_count = num_headers;
 
-	/*
-	hlphr_header headers_s[num_headers];
-
-	int i = 0;
-	for(i = 0; i != num_headers; ++i) {
-		headers_s[i].name = (vbyte*) headers[i].name;
-		headers_s[i].value = (vbyte*) headers[i].value;
-	}
+	ret->headers = hl_alloc_array(&hlt_dynobj, (int)num_headers);
 	
+	for(int i = 0; i < num_headers; i++) {
+		vdynamic *header = (vdynamic*) hl_alloc_dynobj();
+		hl_dyn_setp(header, hl_hash_utf8("name"), &hlt_bytes, hl_copy_bytes(headers[i].name, headers[i].name_len) );
+		hl_dyn_setp(header, hl_hash_utf8("value"), &hlt_bytes, hl_copy_bytes(headers[i].value, headers[i].value_len) );
 
-	ret->headers->headers = headers_s;
-	ret->headers->header_count = num_headers;
-	*/
+		hl_aptr(ret->headers, vdynamic*)[i] = header;
+	}
 
 	return ret;
 }
 
 HL_PRIM void HL_NAME(free_pico_data)(hlphr_data * data) {
-	// free(data->headers);
 	free(data);
 }
 
-#define PHR_DATA _OBJ(_BYTES _BYTES _I32 _I32 _DYN)
+#define PHR_DATA _OBJ(_BYTES _BYTES _I32 _I32 _I32 _ARR)
 DEFINE_PRIM(PHR_DATA, parse_request, _BYTES _I32);
 DEFINE_PRIM(_VOID, free_pico_data, PHR_DATA);
--- picohttp/PicoHttpParser.hx
+++ picohttp/PicoHttpParser.hx
@@ -1,18 +1,50 @@
 package picohttp;
 
-class ParsedRequest {
+typedef ParsedRequest = {
+	var method:String;
+	var path:String;
+	var request_size:Int;
+	var minor_verison:Int;
+	var header_count:Int;
+	var headers:Map<String,String>;
+}
+
+abstract class NativeParsedRequest {
 	public var method:hl.Bytes;
 	public var path:hl.Bytes;
 	public var request_size:Int;
 	public var minor_version:Int;
-	public var headers:Dynamic;
+	public var header_count:Int;
+	public var headers:hl.NativeArray<Dynamic>;
 }
 
 @:hlNative("picohttpparser")
 class PicoHttpParser {
+	public static function parseRequest(request:String, ?max_headers:Int=100):ParsedRequest {
+		var headers : Map<String,String> = new Map<String, String>();
+		var buffer = hl.Bytes.fromBytes(haxe.io.Bytes.ofString(request));
+		var data = parse_request(buffer, max_headers);
+
+		for(header in data.headers) {
+			headers.set( @:privateAccess String.fromUTF8(header.name), 
+				@:privateAccess String.fromUTF8(header.value) );
+		}
+
+		var ret : ParsedRequest = {
+			method: @:privateAccess String.fromUTF8(data.method),
+			path: @:privateAccess String.fromUTF8(data.path),
+			request_size: data.request_size,
+			minor_verison: data.minor_version,
+			header_count: data.header_count,
+			headers: headers
+		};
+		free_pico_data(data);
+		return ret;
+	}
+
 	@:hlNative("?picohttpparser", "parse_request")
-	public static function parse_request(buffer:hl.Bytes, max_header:Int):ParsedRequest {return null;}
+	static function parse_request(buffer:hl.Bytes, max_header:Int):NativeParsedRequest {return null;}
 
 	@:hlNative("?picohttpparser", "free_pico_data")
-	public static function free_pico_data(parsed_data:ParsedRequest):Void {}
+	static function free_pico_data(parsed_data:NativeParsedRequest):Void {}
 }
